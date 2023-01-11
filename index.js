41a5cced3b3dd4c9c7e42cdeb96287322ba3bcdb
Marques Ivory <Maivory12@icloud.com>
2023-01-10 19:57:01 -0500

    Added packages required for application to index.js file

---
+++ index.js
@@ -0,0 +1,16 @@
+// Packages needed for this application
+
+const fs = require("fs")
+const inquirer = require("inquirer")
+
+// Package for team memebers
+
+const employee = require("./library/employee")
+const engineer = require("./library/engineer")
+const intern = require("./library/intern")
+const manager = require("./library/manager")
+
+// Package for generated html file
+
+const generatedHtml = require("./src/genereatedHtml")
+
