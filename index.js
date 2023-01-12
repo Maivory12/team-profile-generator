bbc1191a19da0fbf8f4f0be6de94fbd03c9ae17d
Marques Ivory <Maivory12@icloud.com>
2023-01-11 19:24:36 -0500

    Added prompts for adding a manager to team

--- index.js
+++ index.js
@@ -14,3 +14,39 @@ const manager = require("./library/manager")
 
 const generatedHtml = require("./src/genereatedHtml")
 
+// Team members
+const team = [];
+
+// Prompts for adding a manager
+const addManager = () => {
+    return inquirer.prompt([
+        {
+            type:"input",
+            name:"name",
+            message:"What is the name of the manager of the team?",
+        },
+        {
+            type:"input",
+            name:"ID",
+            message:"What is the manager employee ID?",
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"what is the manager email address?",
+        },
+        {
+            type:"input",
+            name:"officeNumber",
+            message:"What is the office number?",
+        },
+    ])
+        .then (managerInput=>{
+            const {name, ID, email, officeNumber} = managerInput;
+            const manager = new Maneger (name, ID, email, officeNumber);
+            team.push(manager);
+            console.log(manager);
+        })
+    
+}
+
