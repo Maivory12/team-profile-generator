baac6b69dd73d97f72020d38dcb7bc6751f5bd58
Marques Ivory <Maivory12@icloud.com>
2023-01-25 18:27:04 -0500

    Created export function to generate newHtml page

--- src/genereatedHtml.js
+++ src/genereatedHtml.js
@@ -79,4 +79,38 @@ const generateTeam = (team) => {
     return html.join('');
 }
 
+// export function to generate entire page
+module.exports = team => {
+
+    return `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <title>My Team</title>
+</head>
+<body>
+
+    <nav class="navbar navbar-expand-sm bg-light navbar-light justify-content-center navText"> 
+    
+        <h1>My Team</h1>
+
+    </nav>
+
+    <main
+        <div class="container-fluid card-columns d-flex">
+
+                ${generateTeam(team)} 
+                
+        </div>
+    </main>
+     
+</body>
+</html>
+    `;
+}
 
