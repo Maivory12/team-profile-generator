c03a504e1ebe74a82903acd55602f14838c1fad0
Marques Ivory <Maivory12@icloud.com>
2023-01-25 18:16:37 -0500

    Created card to generated engineer and intern input from user

--- src/genereatedHtml.js
+++ src/genereatedHtml.js
@@ -21,4 +21,45 @@ const generateTeam = (team) => {
         `;
         html.push(managerHtml);
     }
+    const generateEngineer = engineer => {
+        console.log(engineer);
+        let engineerHtml = ` 
+       
+        <div class="container mt-3">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="associateName">${engineer.name}</h4>
+                    <h5 class="associateTitle">Engineer</h5>
+                    <p class="ID">${engineer.getId ()}</p>
+                    <a href='https://github.com/Maivory12' class="gitHub">${engineer.github}</a>
+                    <br>
+                    <br>
+                    <a href="mailto:${engineer.email}" class="btn btn-primary email">${engineer.email}</a>
+                </div> 
+            </div>
+        </div>
+     
+        `;
+        html.push(engineerHtml);
+    }
+    const generateIntern = intern => {
+        console.log(intern);
+        let internHtml = ` 
+        <div class="container mt-3">
+            <div class="card">
+                <div class="card-body">
+                    <h4 class="associateName">${intern.name}</h4>
+                    <h5 class="associateTitle">Intern</h5>
+                    <p class="ID">ID: ${intern.getId ()}</p>
+                    <p class="school">School: ${intern.school}</p>
+
+                <a href="mailto:${intern.email}" class="btn btn-primary">${intern.email}</a>
+                </div>
+            </div>
+        </div>
+       
+        `;
+        html.push(internHtml);
+    }
+
 }
